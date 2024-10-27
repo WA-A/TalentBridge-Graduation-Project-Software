179ae4ede53efddd6e595bd884718f934f058368
wasan <wasanawwad2020@gmail.com>
2024-10-27 23:43:16 +0200

    View Own Profile

--- GraduationProject1-BackEnd/src/modules/User/User.controller.js
+++ GraduationProject1-BackEnd/src/modules/User/User.controller.js
@@ -28,3 +28,18 @@ export const createProfile = async (req, res) => {
    
 }
 
+//  View Own Profile
+export const ViewOwnProfile = async (req, res) => {  
+    try {
+       const user = await UserModel.findById(req.user.id);
+       if (!user) return res.status(404).json({ message: "User not found" });
+ 
+       res.status(200).json(user);
+    } catch (error) {
+       res.status(500).json({ message: error.message });
+    }
+ };
+ 
+ 
+ 
+ 
