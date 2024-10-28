014f7a10143934467aac4904fe9eca85ca584031
wasan <wasanawwad2020@gmail.com>
2024-10-28 20:58:27 +0200

    Update Project By Senior Created

--- GraduationProject1-BackEnd/src/modules/Projects/Projects.controller.js
+++ GraduationProject1-BackEnd/src/modules/Projects/Projects.controller.js
@@ -28,7 +28,32 @@ export const GetProjectsBySenior = async (req, res) => {
 };
  
  // Edit Own Project Created
+ export const UpdateProjectBySenior = async (req, res) => {
+    const { ProjectId } = req.params; 
+    const CreatedBySenior = req.user._id; 
 
+  
+    const { ProjectName, Description, RequiredSkills } = req.body;
+
+    const updatedProject = await ProjectsModel.findByIdAndUpdate(
+        { _id: ProjectId, CreatedBySenior },
+        { 
+            $set: {
+                ProjectName, 
+                Description, 
+                RequiredSkills 
+            } 
+        },
+        { new: true } 
+    );
+
+   
+    if (!updatedProject) {
+        return res.status(404).json({ message: "Project not found or you are not authorized to modify this project." });
+    }
+
+    return res.status(200).json({ message: "Project updated successfully", project: updatedProject });
+};
 
 // Delete Own Project Created
 
--- GraduationProject1-BackEnd/src/modules/Projects/Projects.router.js
+++ GraduationProject1-BackEnd/src/modules/Projects/Projects.router.js
@@ -7,9 +7,7 @@ import { EndPoints } from "./Projects.Role.js";
 
 router.post('/createproject',auth(EndPoints.CreateProject),ProjectController.CreateProject);
 router.get('/viewownprojectcreated',auth(EndPoints.CreateProject),ProjectController.GetProjectsBySenior);
-
-
-// router.patch('/updateownprofile',auth(EndPoints.CreateProfile),ProjectController.UpdateOwnProfile);
+router.put('/updateownprojectcreated/:ProjectId',auth(EndPoints.CreateProject),ProjectController.UpdateProjectBySenior);
  
 // router.get('/viewpwnprofile',auth(EndPoints.CreateProfile),ProjectController.ViewOwnProfile);
 export default router
