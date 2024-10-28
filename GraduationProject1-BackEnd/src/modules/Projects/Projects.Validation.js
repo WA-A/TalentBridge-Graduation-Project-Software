5873a98c69aa9daa9367eaaee58521343c62af1b
wasan <wasanawwad2020@gmail.com>
2024-10-28 21:51:40 +0200

    Projects Validation

---
+++ GraduationProject1-BackEnd/src/modules/Projects/Projects.Validation.js
@@ -0,0 +1,22 @@
+import Joi from 'joi';
+
+export const CreateProjectSchema = Joi.object({
+    ProjectName: Joi.string().required(),
+    Description: Joi.string().required(),
+    RequiredSkills: Joi.array().items(Joi.string()).required(),
+    Field: Joi.string().required()
+});
+
+export const UpdateProjectSchema = Joi.object({
+    ProjectName: Joi.string().optional(),
+    Description: Joi.string().optional(),
+    RequiredSkills: Joi.array().items(Joi.string()).optional()
+});
+
+export const DeleteProjectSchema = Joi.object({
+    ProjectId: Joi.string().required()
+});
+
+export const GetProjectsByFieldSchema = Joi.object({
+    Field: Joi.string().required()
+});
--- GraduationProject1-BackEnd/src/modules/Projects/Projects.router.js
+++ GraduationProject1-BackEnd/src/modules/Projects/Projects.router.js
@@ -3,12 +3,15 @@ const router = Router();
 import * as ProjectController from './Projects.controller.js';
 import { auth } from "../../MiddleWare/auth.js";
 import { EndPoints } from "./Projects.Role.js";
+import * as schema from './Projects.Validation.js'
+import { Validation } from "../../MiddleWare/Validation.js";
 
 
-router.post('/createproject',auth(EndPoints.CreateProject),ProjectController.CreateProject);
+
+router.post('/createproject',Validation(schema.CreateProjectSchema),auth(EndPoints.CreateProject),ProjectController.CreateProject);
 router.get('/viewownprojectcreated',auth(EndPoints.CreateProject),ProjectController.GetProjectsBySenior);
-router.put('/updateownprojectcreated/:ProjectId',auth(EndPoints.CreateProject),ProjectController.UpdateProjectBySenior);
-router.delete('/deleteownprojectcreated/:ProjectId',auth(EndPoints.CreateProject),ProjectController.DeleteProjectBySenior);
+router.put('/updateownprojectcreated/:ProjectId',Validation(schema.UpdateProjectSchema),auth(EndPoints.CreateProject),ProjectController.UpdateProjectBySenior);
+router.delete('/deleteownprojectcreated/:ProjectId',Validation(schema.DeleteProjectSchema),auth(EndPoints.CreateProject),ProjectController.DeleteProjectBySenior);
+router.get('/viewprojectbyfiled/:Filed',Validation(schema.GetProjectsByFieldSchema),auth(EndPoints.CreateProject),ProjectController.GetProjectsByField);
 
-// router.get('/viewpwnprofile',auth(EndPoints.CreateProfile),ProjectController.ViewOwnProfile);
 export default router
