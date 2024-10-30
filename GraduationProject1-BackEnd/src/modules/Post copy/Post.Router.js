651487a4a43c477f394d45cc31ea6d484e961dde
wasan <wasanawwad2020@gmail.com>
2024-10-30 23:25:16 +0200

    Create Comment

---
+++ GraduationProject1-BackEnd/src/modules/Post copy/Post.Router.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import {auth }from "../../MiddleWare/auth.js";
+import * as PostController from './Post.Controller.js'
+import fileUpload, { FileValue } from "../../../utls/Multer.js";
+import { AsyncHandler } from "../../../utls/CatchError.js";
+const router = Router();
+
+//router.post('/createpost',auth,fileUpload(FileValue.image).single('images'),AsyncHandler(PostController.CreatePost));
+
+export default router;
