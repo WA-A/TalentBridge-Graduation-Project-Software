11e8213d0e562b303fd40577d48007cc4dffac7d
wasan <wasanawwad2020@gmail.com>
2024-10-31 00:18:37 +0200

    Create Post

--- GraduationProject1-BackEnd/src/modules/auth/auth.router.js
+++ GraduationProject1-BackEnd/src/modules/auth/auth.router.js
@@ -4,15 +4,13 @@ import * as AuthController from './auth.controller.js'
 import { CheckEmail } from "../../MiddleWare/CheckEmail.js";
 import { AsyncHandler } from "../../../utls/CatchError.js";
 import { Validation } from "../../MiddleWare/Validation.js";
-import * as schema from './Auth.Validation.js'
-import fileUpload from "../../../utls/Multer.js";
+import * as schema from './Auth.Validation.js';
 
 
 
 
 
 router.post('/signup',Validation(schema.RegisterSchema),CheckEmail,AuthController.SignUp);
-router.post('/addexcle',fileUpload().single('excle'),AsyncHandler(AuthController.AddUserExcel));
 router.post('/signin',Validation(schema.LoginSchema),AuthController.SignIn);
 router.patch('/sendcode',Validation(schema.SendCodeSchema),AuthController.SendCode);
 router.patch('/forgotpassword',Validation(schema.ForgetPasswordSchema),AuthController.ForgotPassword);
