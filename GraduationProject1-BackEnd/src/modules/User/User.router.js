f12c7a79e1f6984ad3854237f290f60f3e0cd7f2
wasan <wasanawwad2020@gmail.com>
2024-11-15 22:04:37 +0200

    Enhace Validation MiddleWare

--- GraduationProject1-BackEnd/src/modules/User/User.router.js
+++ GraduationProject1-BackEnd/src/modules/User/User.router.js
@@ -4,6 +4,9 @@ import * as UserController from './User.controller.js'
 import fileUpload, { FileValue } from "../../../utls/Multer.js";
 import { auth } from "../../MiddleWare/auth.js";
 import { EndPoints } from "./User.Role.js";
+import { Validation } from "../../MiddleWare/Validation.js";
+import * as schema from './User.Validation.js';
+
 
 
 router.post('/createprofile',auth(EndPoints.CreateProfile),fileUpload(FileValue.image).single('PictureProfile'),UserController.CreateProfile);
