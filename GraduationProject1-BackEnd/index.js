6b3d7699945281d38811e44be543768ff06e6d9b
wasan <wasanawwad2020@gmail.com>
2024-11-18 21:57:17 +0200

    Test Join API With FrontPage

--- GraduationProject1-BackEnd/index.js
+++ GraduationProject1-BackEnd/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import Appinit from './src/app.router.js';
 
 dotenv.config();
 const app = express()
 const PORT = process.env.PORT || 3000;
+
+
+app.use(cors());
 
 Appinit(app,express);
 
@@ -12,3 +16,5 @@ Appinit(app,express);
 app.listen(PORT,()=>{
     console.log(`server is running ..... ${PORT}`);
     });
+
+   
