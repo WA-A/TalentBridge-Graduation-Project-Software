b37af3b096413a275599d1f682abbf2b84d468d1
wasan <wasanawwad2020@gmail.com>
2024-12-21 21:14:53 +0200

    External Api Skills

--- GraduationProject1-BackEnd/ExternalApi/ExternalApi.router.js
+++ GraduationProject1-BackEnd/ExternalApi/ExternalApi.router.js
@@ -4,5 +4,6 @@ import * as ExternalApiController  from './ExternealApi.controller.js';
 const router = express.Router();
 
 router.get('/getlanguages',ExternalApiController.GetLanguages);
+router.get('/getskills',ExternalApiController.GetSkills);
 
 export default router;
