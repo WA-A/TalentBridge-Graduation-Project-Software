bf751d8af94973e0fe1320cb64c4739993fb773f
wasan <wasanawwad2020@gmail.com>
2024-12-23 22:47:45 +0200

    Get User Skills

--- GraduationProject1-BackEnd/ExternalApiSkills/ExternealApiSkills.controller.js
+++ GraduationProject1-BackEnd/ExternalApiSkills/ExternealApiSkills.controller.js
@@ -332,3 +332,39 @@ export const GetSkills = (req, res) => {
         return res.status(500).json({ message: "Internal Server Error" });
     }
 };
+
+
+export const GetUserSkills = async (req, res) => {
+    try {
+        if (!req.user) {
+            console.log("User not authorized: No token provided.");
+            return res.status(401).json({ message: "User not authorized. Please provide a valid token." });
+        }
+
+        const authUser = req.user;
+
+        const user = await UserModel.findById(authUser);
+        if (!user) {
+            console.log("User not found in the database.");
+            return res.status(404).json({ message: "User not found." });
+        }
+
+        console.log("User found:", user);
+
+        const userSkills = user.Skills;
+
+        if (userSkills.length === 0) {
+            return res.status(404).json({ message: "No skills added yet." });
+        }
+
+        console.log("User skills:", userSkills);
+        return res.status(200).json({
+            message: "User skills fetched successfully.",
+            skills: userSkills
+        });
+
+    } catch (error) {
+        console.error("Error fetching user skills: ", error);
+        return res.status(500).json({ message: "Internal Server Error." });
+    }
+};
