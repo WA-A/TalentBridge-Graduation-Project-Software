54b8769c668be402aa66067404c73947c6adc960
wasan <wasanawwad2020@gmail.com>
2025-01-25 00:01:23 +0200

    Admin Accept of Senior Request

--- GraduationProject1-BackEnd/src/modules/Admin/Admin.controller.js
+++ GraduationProject1-BackEnd/src/modules/Admin/Admin.controller.js
@@ -1,5 +1,6 @@
 import RequestSeniorToAdminModel from '../../Model/RequestSeniorToAdminModel.js';
-
+import UserModel from '../../Model/User.Model.js'; 
+import { SendEmail } from "../../../utls/SendEmail.js";
 
 // Get All Request Senior To Admin
 
@@ -20,6 +21,37 @@ export const GetAllRequestSeniorToAdmin = async (req, res) => {
     }
 }
 
+
+// Admin Accept of Senior Request
+export const AdminAcceptofSeniorRequest = async (req, res) => {
+    try {
+        const { UserId } = req.params;
+
+        const user = await UserModel.findByIdAndUpdate(
+            UserId,
+            { SeniorAccountStatus: 'Active' },
+            { new: true } 
+        );
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        await SendEmail(
+            user.Email, 
+            `Hello ${user.FullName},
+            I hope you are well.
+            Your request has been approved by the Talent Bridge admin to become eligible to be a senior.
+            You can now log in to your account through the login page.`
+        );
+
+        res.status(200).json({ message: 'Senior account approved and email sent successfully',
+            user: user});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'An error occurred while processing the request' });
+    }
+};
 
  
  
